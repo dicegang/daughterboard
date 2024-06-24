7e6fef7b7293c4d43b65baf38f5865f25ff0d677
Ethan Wu <ethanwu10@gmail.com>
2024-06-24 03:04:18 -0400

    wip MSP3461 driver

--- board-firmware/main/MCP3461.c
+++ board-firmware/main/MCP3461.c
@@ -1,6 +1,316 @@
-#include <hal/spi_hal.h>
 #include "MCP3461.h"
+#include <assert.h>
+#include <driver/spi_common.h>
+#include <driver/spi_master.h>
+#include <esp_err.h>
+#include <string.h>
 
-static void mcp3461_init(spi_host_device_t session) {
+#define LOG_TAG "MCP3461"
 
+#define sizeof_member(type, member) (sizeof(((type *)0)->member))
+
+#define BIT_FIELDn(n, x, bits, pos)                                            \
+	((((uint##n##_t)(x)) & ((1 << bits) - 1)) << pos)
+#define BIT_FIELD8(x, bits, pos) BIT_FIELDn(8, x, bits, pos)
+
+static inline int32_t sign_extend32(uint32_t num, size_t n_bits) {
+	size_t n_extra_sign_bits = 32 - n_bits;
+	return ((int32_t)(num << n_extra_sign_bits)) >> n_extra_sign_bits;
+}
+
+typedef enum : uint8_t {
+	COMMAND_TYPE_FAST = 0b00,
+	COMMAND_TYPE_STATIC_READ = 0b01,
+	COMMAND_TYPE_INC_READ = 0b11,
+	COMMAND_TYPE_INC_WRITE = 0b10,
+} command_type_t;
+
+typedef enum : uint8_t {
+	FAST_COMMAND_ADC_CONV_START = 0b1010,
+	FAST_COMMAND_ADC_STANDBY_MODE = 0b1011,
+	FAST_COMMAND_ADC_SHUTDOWN_MODE = 0b1100,
+	FAST_COMMAND_FULL_SHUTDOWN_MODE = 0b1101,
+	FAST_COMMAND_FULL_RESET = 0b1110,
+} fast_command_t;
+
+typedef enum : uint8_t {
+	REG_ADCDATA = 0x00,
+	REG_CONFIG0 = 0x01,
+	REG_CONFIG1 = 0x02,
+	REG_CONFIG2 = 0x03,
+	REG_CONFIG3 = 0x04,
+	REG_IRQ = 0x05,
+	REG_MUX = 0x06,
+	REG_SCAN = 0x07,
+	REG_TIMER = 0x08,
+	REG_OFFSETCAL = 0x09,
+	REG_GAINCAL = 0x0A
+} mcp3461_register;
+
+typedef struct __attribute__((packed)) {
+	command_type_t type : 2;
+	uint8_t reg_addr_or_fast_cmd : 4;
+	uint8_t device_addr : 2;
+} command_byte_t;
+_Static_assert(sizeof(command_byte_t) == 1, "sizeof(command_byte_t)");
+
+esp_err_t mcp3461_create(mcp3461_device_t *h, spi_host_device_t host_id,
+                         int cs_io_num, uint8_t device_address) {
+	esp_err_t ret = ESP_OK;
+
+	spi_device_interface_config_t dev_config = {
+		.command_bits = 8,
+		.address_bits = 0,
+		.dummy_bits = 0,
+		.mode = 0, // (CPOL, CPHA) = (0, 0)
+		.clock_source = SPI_CLK_SRC_DEFAULT,
+		.clock_speed_hz = SPI_CLOCK_RATE, // FIXME(etw): configurable?
+		.spics_io_num = cs_io_num,
+		.flags = 0,
+		.queue_size = 1,
+	};
+	if ((ret = spi_bus_add_device(host_id, &dev_config, &h->spidev)) !=
+	    ESP_OK) {
+		goto done;
+	}
+
+	memset(h, 0, sizeof(*h));
+	h->device_address = device_address;
+
+done:
+	return ret;
+}
+
+esp_err_t mcp3461_dispose(mcp3461_device_t *h) {
+	esp_err_t ret = ESP_OK;
+
+	if ((ret = spi_bus_remove_device(h->spidev)) != ESP_OK) {
+		goto done;
+	}
+
+	h->spidev = NULL;
+
+done:
+	return ret;
+}
+
+static inline command_byte_t empty_command_byte(const mcp3461_device_t *h) {
+	command_byte_t ret = {
+		.device_addr = h->device_address,
+	};
+	return ret;
+}
+
+static esp_err_t write_reg_buf(const mcp3461_device_t *h, uint8_t reg_addr,
+                               const void *data, size_t bit_len) {
+	esp_err_t ret = ESP_OK;
+
+	command_byte_t command_byte = empty_command_byte(h);
+	command_byte.type = COMMAND_TYPE_INC_WRITE;
+	command_byte.reg_addr_or_fast_cmd = reg_addr;
+	spi_transaction_t transaction = {
+		.length = bit_len,
+		.tx_buffer = data,
+		.rx_buffer = NULL,
+	};
+	__builtin_memcpy(&transaction.cmd, &command_byte, sizeof(command_byte));
+	if ((ret = spi_device_transmit(h->spidev, &transaction)) != ESP_OK) {
+		goto done;
+	}
+
+done:
+	return ret;
+}
+
+static esp_err_t write_reg_int(const mcp3461_device_t *h, uint8_t reg_addr,
+                               const uint32_t data, unsigned bit_len) {
+	esp_err_t ret = ESP_OK;
+
+	static_assert(sizeof(data) <= sizeof_member(spi_transaction_t, tx_buffer),
+	              "data does not fit in tx buffer");
+
+	if (bit_len > sizeof_member(spi_transaction_t, tx_buffer) * 8) {
+		ret = ESP_ERR_INVALID_SIZE;
+		goto done;
+	}
+
+	command_byte_t command_byte = empty_command_byte(h);
+	command_byte.type = COMMAND_TYPE_INC_WRITE;
+	command_byte.reg_addr_or_fast_cmd = reg_addr;
+	spi_transaction_t transaction = {
+		.flags = SPI_TRANS_USE_TXDATA,
+		.length = bit_len,
+		.rx_buffer = NULL,
+		.tx_data = {0},
+	};
+	__builtin_memcpy(&transaction.cmd, &command_byte, sizeof(command_byte));
+
+	// copy `data` as a `bit_len` big endian integer into the TX buffer,
+	// with tx_buffer being a sequence of MSB-first bytes
+
+	uint32_t data_formatted = SPI_SWAP_DATA_TX(data, bit_len);
+	// check that the memcpy is in bounds
+	static_assert(sizeof(data_formatted) <= sizeof(transaction.tx_data),
+	              "data_formatted does not fit in tx buffer");
+	__builtin_memcpy(transaction.tx_data, &data_formatted,
+	                 sizeof(data_formatted));
+
+	if ((ret = spi_device_transmit(h->spidev, &transaction)) != ESP_OK) {
+		goto done;
+	}
+
+done:
+	return ret;
+}
+
+static esp_err_t read_reg_buf(const mcp3461_device_t *h, uint8_t reg_addr,
+                              void *data, size_t bit_len) {
+	esp_err_t ret = ESP_OK;
+
+	command_byte_t command_byte = empty_command_byte(h);
+	command_byte.type = COMMAND_TYPE_INC_READ;
+	command_byte.reg_addr_or_fast_cmd = reg_addr;
+	spi_transaction_t transaction = {
+		.length = bit_len,
+		.rxlength = bit_len,
+		.tx_buffer = NULL,
+		.rx_buffer = data,
+	};
+	__builtin_memcpy(&transaction.cmd, &command_byte, sizeof(command_byte));
+	if ((ret = spi_device_transmit(h->spidev, &transaction)) != ESP_OK) {
+		goto done;
+	}
+
+done:
+	return ret;
+}
+
+static esp_err_t read_reg_int(const mcp3461_device_t *h, uint8_t reg_addr,
+                              uint32_t *data, unsigned bit_len) {
+	esp_err_t ret = ESP_OK;
+
+	static_assert(sizeof(data) <= sizeof_member(spi_transaction_t, rx_buffer),
+	              "data does not fit in rx buffer");
+
+	if (bit_len > sizeof_member(spi_transaction_t, rx_buffer) * 8) {
+		ret = ESP_ERR_INVALID_SIZE;
+		goto done;
+	}
+
+	command_byte_t command_byte = empty_command_byte(h);
+	command_byte.type = COMMAND_TYPE_INC_READ;
+	command_byte.reg_addr_or_fast_cmd = reg_addr;
+	spi_transaction_t transaction = {
+		.length = bit_len,
+		.flags = SPI_TRANS_USE_RXDATA,
+		.rxlength = bit_len,
+		.tx_buffer = NULL,
+		.rx_data = {0},
+	};
+	__builtin_memcpy(&transaction.cmd, &command_byte, sizeof(command_byte));
+	if ((ret = spi_device_transmit(h->spidev, &transaction)) != ESP_OK) {
+		goto done;
+	}
+
+	uint32_t raw_data;
+	static_assert(sizeof(raw_data) == sizeof(*data),
+	              "raw_data doesn't match data");
+	static_assert(sizeof(raw_data) <= sizeof(transaction.rx_data),
+	              "rx_data is too small for data");
+	__builtin_memcpy(&raw_data, transaction.rx_data, sizeof(raw_data));
+	*data = SPI_SWAP_DATA_RX(raw_data, bit_len);
+
+done:
+	return ret;
+}
+
+static esp_err_t do_fast_cmd(const mcp3461_device_t *h,
+                             fast_command_t command) {
+	esp_err_t ret = ESP_OK;
+
+	command_byte_t command_byte = empty_command_byte(h);
+	command_byte.type = COMMAND_TYPE_INC_READ;
+	command_byte.reg_addr_or_fast_cmd = command;
+	spi_transaction_t transaction = {
+		.length = 0,
+		.tx_buffer = NULL,
+		.rx_buffer = NULL,
+	};
+	__builtin_memcpy(&transaction.cmd, &command_byte, sizeof(command_byte));
+	if ((ret = spi_device_transmit(h->spidev, &transaction)) != ESP_OK) {
+		goto done;
+	}
+
+done:
+	return ret;
+}
+
+// hidden for now since we can't handle the device in its default
+// configuration
+static esp_err_t mcp3461_reset(mcp3461_device_t *h) {
+	return do_fast_cmd(h, FAST_COMMAND_FULL_RESET);
+}
+
+esp_err_t mcp3461_init(mcp3461_device_t *h) {
+	esp_err_t ret = ESP_OK;
+
+	if ((ret = mcp3461_reset(h)) != ESP_OK) {
+		goto done;
+	}
+
+	if ((ret = write_reg_int(h, REG_CONFIG3,
+	                         adc_config3(MCP3461_CONV_CONTINUOUS, 0, 0), 8)) !=
+	    ESP_OK) {
+		goto done;
+	}
+
+done:
+	return ret;
+}
+
+esp_err_t mcp3461_set_adc_mode(mcp3461_device_t *h, adc_mode mode) {
+	fast_command_t fast_cmd;
+	switch (mode) {
+	case MCP3461_ADC_MODE_SHUTDOWN:
+		fast_cmd = FAST_COMMAND_ADC_SHUTDOWN_MODE;
+		break;
+	case MCP3461_ADC_MODE_STANDBY:
+		fast_cmd = FAST_COMMAND_ADC_STANDBY_MODE;
+		break;
+	case MCP3461_ADC_MODE_CONVERSION:
+		fast_cmd = FAST_COMMAND_ADC_CONV_START;
+		break;
+	default:
+		return ESP_ERR_INVALID_ARG;
+	}
+
+	return do_fast_cmd(h, fast_cmd);
+}
+
+esp_err_t mcp3461_set_scan(mcp3461_device_t *h, scan_chan channels) {
+	return write_reg_int(h, REG_SCAN, channels, 24);
+}
+
+esp_err_t mcp3461_set_mux(mcp3461_device_t *h, mux_pin positive,
+                          mux_pin negative) {
+	uint8_t reg_value = BIT_FIELD8(positive, 4, 4) | BIT_FIELD8(negative, 4, 0);
+	return write_reg_int(h, REG_MUX, reg_value, 8);
+}
+
+esp_err_t mcp3461_read(mcp3461_device_t *h, mcp3461_read_data_t *out) {
+	esp_err_t ret = ESP_OK;
+
+	// TODO: support other data formats (only 17 bit with channel ID currently
+	// supported)
+
+	uint32_t raw_data;
+	if ((ret = read_reg_int(h, REG_ADCDATA, &raw_data, 32)) != ESP_OK) {
+		goto done;
+	}
+
+	out->value = sign_extend32(raw_data, 17);
+	out->channel_id = raw_data >> 28;
+
+done:
+	return ret;
 }
--- board-firmware/main/MCP3461.h
+++ board-firmware/main/MCP3461.h
@@ -1,130 +1,134 @@
-//
-// Created by alec on 6/2/24.
-//
+/// @file MCP3461.h
+/// @brief Driver for the MCP3461/2/4-R family of ADCs
 
 #ifndef ESP_NOW_RELAY_MCP3461_H
 #define ESP_NOW_RELAY_MCP3461_H
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
+#include <driver/spi_master.h>
+#include <esp_err.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// XXX(etw): avrisp runs at 1/6 this speed
 #define SPI_CLOCK_RATE 1000000
 
 typedef enum {
-	REG_ADCDATA = 0x00,
-	REG_CONFIG0 = 0x01,
-	REG_CONFIG1 = 0x02,
-	REG_CONFIG2 = 0x03,
-	REG_CONFIG3 = 0x04,
-	REG_IRQ = 0x05,
-	REG_MUX = 0x06,
-	REG_SCAN = 0x07,
-	REG_TIMER = 0x08,
-	REG_OFFSETCAL = 0x09,
-	REG_GAINCAL = 0x0A
-} mcp3461_register;
-
-typedef enum {
-	ADC_MODE_SHUTDOWN = 0b00,
-	ADC_MODE_STANDBY = 0b10,
-	ADC_MODE_CONVERSION = 0b11
+	MCP3461_ADC_MODE_SHUTDOWN = 0b00,
+	MCP3461_ADC_MODE_STANDBY = 0b10,
+	MCP3461_ADC_MODE_CONVERSION = 0b11
 } adc_mode;
 
 typedef enum {
-	ADC_BIAS_NONE,
-	ADC_BIAS_0p9uA,
-	ADC_BIAS_3p7uA,
-	ADC_BIAS_15p0uA
+	MCP3461_ADC_BIAS_NONE,
+	MCP3461_ADC_BIAS_0p9uA,
+	MCP3461_ADC_BIAS_3p7uA,
+	MCP3461_ADC_BIAS_15p0uA
 } adc_bias;
 
 typedef enum {
-	CLK_SEL_EXTERNAL = 0b00,
-	CLK_SEL_INTERNAL = 0b10,
-	CLK_SEL_INTERNAL_AMCLK = 0b11
+	MCP3461_CLK_SEL_EXTERNAL = 0b00,
+	MCP3461_CLK_SEL_INTERNAL = 0b10,
+	MCP3461_CLK_SEL_INTERNAL_AMCLK = 0b11
 } clk_sel;
 
 typedef enum {
-	OSR_32 = 0b0000,
-	OSR_64 = 0b0001,
-	OSR_128 = 0b0010,
-	OSR_256 = 0b0011,
-	OSR_512 = 0b0100,
-	OSR_1024 = 0b0101,
-	OSR_2048 = 0b0110,
-	OSR_4096 = 0b0111,
-	OSR_8192 = 0b1000,
-	OSR_16384 = 0b1001,
-	OSR_20480 = 0b1010,
-	OSR_24576 = 0b1011,
-	OSR_40960 = 0b1100,
-	OSR_49152 = 0b1101,
-	OSR_81920 = 0b1110,
-	OSR_98304 = 0b1111
+	MCP3461_OSR_32 = 0b0000,
+	MCP3461_OSR_64 = 0b0001,
+	MCP3461_OSR_128 = 0b0010,
+	MCP3461_OSR_256 = 0b0011,
+	MCP3461_OSR_512 = 0b0100,
+	MCP3461_OSR_1024 = 0b0101,
+	MCP3461_OSR_2048 = 0b0110,
+	MCP3461_OSR_4096 = 0b0111,
+	MCP3461_OSR_8192 = 0b1000,
+	MCP3461_OSR_16384 = 0b1001,
+	MCP3461_OSR_20480 = 0b1010,
+	MCP3461_OSR_24576 = 0b1011,
+	MCP3461_OSR_40960 = 0b1100,
+	MCP3461_OSR_49152 = 0b1101,
+	MCP3461_OSR_81920 = 0b1110,
+	MCP3461_OSR_98304 = 0b1111
 } adc_osr;
 
 typedef enum {
-	GAIN_THIRD = 0b000,
-	GAIN_1 = 0b001,
-	GAIN_2 = 0b010,
-	GAIN_4 = 0b011,
-	GAIN_8 = 0b100,
-	GAIN_16 = 0b101,
-	GAIN_32 = 0b110,
-	GAIN_64 = 0b111
+	MCP3461_GAIN_THIRD = 0b000,
+	MCP3461_GAIN_1 = 0b001,
+	MCP3461_GAIN_2 = 0b010,
+	MCP3461_GAIN_4 = 0b011,
+	MCP3461_GAIN_8 = 0b100,
+	MCP3461_GAIN_16 = 0b101,
+	MCP3461_GAIN_32 = 0b110,
+	MCP3461_GAIN_64 = 0b111
 } adc_gain;
 
 typedef enum {
-	CONV_ONE_SHOT_SHUTDOWN = 0b00,
-	CONV_ONE_SHOT_STANDBY = 0b10,
-	CONV_CONTINUOUS = 0b11
+	MCP3461_CONV_ONE_SHOT_SHUTDOWN = 0b00,
+	MCP3461_CONV_ONE_SHOT_STANDBY = 0b10,
+	MCP3461_CONV_CONTINUOUS = 0b11
 } conv_mode;
 
 typedef enum {
-	MUX_CH0 = 0b0000,
-	MUX_CH1 = 0b0001,
-	MUX_CH2 = 0b0010,
-	MUX_CH3 = 0b0011,
-	MUX_CH4 = 0b0100,
-	MUX_CH5 = 0b0101,
-	MUX_CH6 = 0b0110,
-	MUX_CH7 = 0b0111,
+	MCP3461_MUX_CH0 = 0b0000,
+	MCP3461_MUX_CH1 = 0b0001,
+	MCP3461_MUX_CH2 = 0b0010,
+	MCP3461_MUX_CH3 = 0b0011,
+	MCP3461_MUX_CH4 = 0b0100,
+	MCP3461_MUX_CH5 = 0b0101,
+	MCP3461_MUX_CH6 = 0b0110,
+	MCP3461_MUX_CH7 = 0b0111,
 
-	MUX_AGND = 0b1000,
-	MUX_AVDD = 0b1001,
-	MUX_REFINP = 0b1011,
-	MUX_REFINM = 0b1100,
+	MCP3461_MUX_AGND = 0b1000,
+	MCP3461_MUX_AVDD = 0b1001,
+	MCP3461_MUX_REFINP = 0b1011,
+	MCP3461_MUX_REFINM = 0b1100,
 
-	MUX_TEMP_P = 0b1101,
-	MUX_TEMP_M = 0b1110,
+	MCP3461_MUX_TEMP_P = 0b1101,
+	MCP3461_MUX_TEMP_M = 0b1110,
 
-	MUX_VCM = 0b1111
+	MCP3461_MUX_VCM = 0b1111
 } mux_pin;
 
 #define __BIT(n) (1 << (n))
 typedef enum {
-	CHAN_CH0 = __BIT(0b0000),
-	CHAN_CH1 = __BIT(0b0001),
-	CHAN_CH2 = __BIT(0b0010),
-	CHAN_CH3 = __BIT(0b0011),
-	CHAN_CH4 = __BIT(0b0100),
-	CHAN_CH5 = __BIT(0b0101),
-	CHAN_CH6 = __BIT(0b0110),
-	CHAN_CH7 = __BIT(0b0111),
+	MCP3461_CHAN_CH0 = __BIT(0b0000),
+	MCP3461_CHAN_CH1 = __BIT(0b0001),
+	MCP3461_CHAN_CH2 = __BIT(0b0010),
+	MCP3461_CHAN_CH3 = __BIT(0b0011),
+	MCP3461_CHAN_CH4 = __BIT(0b0100),
+	MCP3461_CHAN_CH5 = __BIT(0b0101),
+	MCP3461_CHAN_CH6 = __BIT(0b0110),
+	MCP3461_CHAN_CH7 = __BIT(0b0111),
 
-	CHAN_0_1 = __BIT(0b1000),
-	CHAN_2_3 = __BIT(0b1001),
-	CHAN_4_5 = __BIT(0b1010),
-	CHAN_6_7 = __BIT(0b1011),
+	MCP3461_CHAN_0_1 = __BIT(0b1000),
+	MCP3461_CHAN_2_3 = __BIT(0b1001),
+	MCP3461_CHAN_4_5 = __BIT(0b1010),
+	MCP3461_CHAN_6_7 = __BIT(0b1011),
 
-	CHAN_TEMP = __BIT(0b1100),
-	CHAN_AVDD = __BIT(0b1101),
-CHAN_VCM = __BIT(0b1110),
-CHAN_OFF = __BIT(0b1111)
+	MCP3461_CHAN_TEMP = __BIT(0b1100),
+	MCP3461_CHAN_AVDD = __BIT(0b1101),
+	MCP3461_CHAN_VCM = __BIT(0b1110),
+	MCP3461_CHAN_OFF = __BIT(0b1111)
 } scan_chan;
 #undef __BIT
 
-inline uint8_t adc_config0(adc_mode mode, adc_bias bias, bool partial_shutdown, clk_sel clk, bool internal_vref) {
-	return (internal_vref << 7) | (partial_shutdown << 6) | (clk << 4) | (bias << 2) | mode;
+typedef struct __attribute__((packed)) {
+	adc_mode mode : 2;
+	adc_bias bias : 2;
+	clk_sel clk_sel : 2;
+	bool partial_shutdown_n : 1;
+	bool vref_sel : 1;
+} mcp3461_config0;
+static_assert(sizeof(mcp3461_config0) == 1, "CONFIG0 register size");
+
+inline uint8_t adc_config0(adc_mode mode, adc_bias bias, bool partial_shutdown,
+                           clk_sel clk, bool internal_vref) {
+	return (internal_vref << 7) | (partial_shutdown << 6) | (clk << 4) |
+	       (bias << 2) | mode;
 }
 
 inline uint8_t adc_config1(adc_osr osr) {
@@ -135,23 +139,104 @@ inline uint8_t adc_config2(adc_gain gain) {
 	return (0b10 << 6) | (gain << 3) | (0b0 << 2) | (0b0 << 1) | 0b1;
 }
 
-inline uint8_t adc_config3(conv_mode conv_mode, bool offset_cal, bool gain_cal) {
+// (32768 * gain) * offsetcal_volts / V_ref = offsetcal_bits
+// (32768 * 1) * -1.65 / 3.3
+inline uint8_t adc_config3(conv_mode conv_mode, bool offset_cal,
+                           bool gain_cal) {
+	// DATA_FORMAT set to 0b11 (17 bits + channel ID)
 	return (conv_mode << 7) | (0b11 << 5) | (offset_cal << 1) | gain_cal;
 }
 
-inline uint8_t adc_mux(mux_pin minus, mux_pin plus) {
-	return (minus << 4) | plus;
+inline uint8_t adc_mux(mux_pin plus, mux_pin minus) {
+	return (plus << 4) | minus;
 }
 
-inline uint32_t adc_scan(uint16_t channels) {
-	return channels;
-}
+inline uint32_t adc_scan(uint16_t channels) { return channels; }
 
 typedef struct {
-	spi_host_device_t spi_host;
-} mcp3461_handle_t;
+	spi_device_handle_t spidev;
+	uint8_t device_address;
+} mcp3461_device_t;
 
-// (32768 * gain) * offsetcal_volts / V_ref = offsetcal_bits
-// (32768 * 1) * -1.65 / 3.3
+/// @brief Create a new MCP346xR device
+///
+/// This function opens a device, and configures the driver state for as if the
+/// device had just been reset. However, no reset or initialization is
+/// performed. `mcp3461_init` should be called to reset and initialize the
+/// device
+///
+/// @param[out] h Device handle to initialize
+/// @param[in]  host_id SPI host peripheral ID that the device is on
+/// @param[in]  cs_io_num CS GPIO pin of the device, or -1 if not used
+/// @param[in]  device_address Address of the device (normal-order parts are 1)
+esp_err_t mcp3461_create(mcp3461_device_t *h, spi_host_device_t host_id,
+                         int cs_io_num, uint8_t device_address);
 
-#endif //ESP_NOW_RELAY_MCP3461_H
+/// @brief Reset and initialize the device
+///
+/// Performs a device reset via a fast command, and then configures the device
+/// to a default state.
+///
+/// @param h Device handle
+esp_err_t mcp3461_init(mcp3461_device_t *h);
+
+/// @brief Tear down the device
+///
+/// Disposes of driver resources associated with the device handle.
+///
+/// @param h Device handle
+esp_err_t mcp3461_dispose(mcp3461_device_t *h);
+
+/// @brief Set the ADC mode
+///
+/// Issues a fast command to set the desired ADC mode. Use for starting
+/// conversions, or placing the device in standby or shutdown.
+///
+/// @param h Device handle
+/// @param mode The mode to set
+esp_err_t mcp3461_set_adc_mode(mcp3461_device_t *h, adc_mode mode);
+
+/// @brief Set the channels enabled in scan
+///
+/// Sets the SCAN register to select which channels are enabled in the ADC's
+/// conversion scan. The `channels` parameter is a bitmask obtained by
+/// bitwise-OR-ing the desired MCP3461_CHAN_* constants for the desired channels
+/// to enable. When `channels` is 0, the device is in mux mode.
+///
+/// @param h Device handle
+/// @param channels A bitmask of enabled channels obtained by |'ing together
+///                 MCP3461_CHAN_* constants
+esp_err_t mcp3461_set_scan(mcp3461_device_t *h, scan_chan channels);
+
+/// @brief Set the mux inputs for mux mode
+///
+/// Sets the MUX register to select mux inputs. Does not bring device into mux
+/// mode if is not already in mux mode. To bring into mux mode, use
+/// `mcp3461_set_scan`.
+///
+/// @param h Device handle
+/// @param positive Pin for VIN+
+/// @param negative Pin for VIN-
+esp_err_t mcp3461_set_mux(mcp3461_device_t *h, mux_pin positive,
+                          mux_pin negative);
+
+// This struct does not necessarily match any register layout, it is just for
+// returning data through the driver API
+typedef struct {
+	int32_t value : 28;
+	unsigned channel_id : 4;
+} mcp3461_read_data_t;
+static_assert(sizeof(mcp3461_read_data_t) == 4, "packing");
+
+/// @brief Read the ADC data
+///
+/// Reads the current data value in the device's output register.
+///
+/// @param h Device handle
+esp_err_t mcp3461_read(mcp3461_device_t *h, mcp3461_read_data_t *out);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // ESP_NOW_RELAY_MCP3461_H
